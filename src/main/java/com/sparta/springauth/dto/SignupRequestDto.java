345863e247ac5b1c5a125343272e3dfd02725562
sajieun <sajieun000721@naver.com>
2025-01-15 20:02:46 +0900

    정규식 표현

--- src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
@@ -1,13 +1,23 @@
 package com.sparta.springauth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SignupRequestDto {
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
+    @Pattern(
+            regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+            message = "유효한 이메일 형식이 아닙니다."
+    )
+    @NotBlank
     private String email;
     private boolean admin = false;
     private String adminToken = "";
